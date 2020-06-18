80980f2ee7cceb995a498887f1af30b1e15da140
geekwhocodes <ganeshraskar@outlook.com>
2020-06-18 20:18:05 +0530

    added publish script

---
+++ .build/Publish.ps1
@@ -0,0 +1,32 @@
+[CmdletBinding()]
+param (
+    [Parameter(Mandatory = $true, HelpMessage = "PowerShell Gallery Api key")]
+    [ValidateNotNull()]
+    [string]
+    $NuGetApiKey
+)
+Begin {
+    Write-Output "Publishing module to PowerShell Gallery"
+    $OutMessage = $null
+}
+Process {
+    try {
+        #TODO: code sign
+        Publish-Module -Path ./SimplePSLogger -NuGetApiKey $NuGetApiKey`
+        -ProjectUri "https://github.com/geekwhocodes/simple-ps-logger"`
+            -Tags "powershell, powershellcore, logging, logger, simplelogging, simplelogger,  pscore, log"`
+            -LicenseUri "https://github.com/geekwhocodes/simple-ps-logger/blob/master/LICENSE"`
+            -ErrorAction Continue
+ 
+        Write-Output "Module published successfully."
+    }
+    catch {
+        $OutMessage = "Error ocurred while publishing module - $($_.Exception.Message)"
+    }
+    finally {
+        Write-Output "$OutMessage"
+    }
+}
+End {
+
+}
