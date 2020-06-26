f7c69a8fc5ad767250092197acb1b738f79bf4ed
geekwhocodes <ganeshraskar@outlook.com>
2020-06-26 23:52:51 +0530

    Added example to use az log analytics using buffersize setting

--- Examples/AzLogAnalytics.ps1
+++ Examples/AzLogAnalytics.ps1
@@ -2,7 +2,7 @@ Import-Module -Name "../SimplePSLogger/SimplePSLogger.psd1"
 
 # In memory conguration
 $SimplePSLoggerConfig = @{
-    Name      = "az-analytics-example"
+    Name      = "az-analytics-example-buffered"
     Providers = @{
         File           = @{
             LiteralFilePath = "G:\Git\simple-ps-logger\Examples\example-with-config-file\example-with-config.log"
@@ -10,32 +10,45 @@ $SimplePSLoggerConfig = @{
             Enabled         = $false
         }
         AzLogAnalytics = @{
-            Enabled  = $true
-            LogLevel = "verbose"
+            Enabled    = $true
+            LogLevel   = "verbose"
             #WorkspaceId  = "****************" # Fetch from your secure store. example - KeyVault
             #WorkspaceKey = "****************" # Fetch from your secure store. example - KeyVault
-            LogType  = "GWCPSLogger" # https://docs.microsoft.com/en-us/azure/azure-monitor/platform/data-collector-api#record-type-and-properties
+            LogType    = "GWCPSLogger" # https://docs.microsoft.com/en-us/azure/azure-monitor/platform/data-collector-api#record-type-and-properties
+            BufferSize = 50
+            Flush      = $true
         }
     }
 }
-
-$MyLogger = New-SimplePSLogger -Configuration $SimplePSLoggerConfig
-
-$MyLogger.Log("fiest log test")
 
 $objectLog = @{
     "k1" = "dsfjlijoiefoiuowfwuoewf"
     "k2" = $obj
     "k3" = @{
-        "k3.k1" = "''ef\ferthew\gkuhweg/qwrqw"
+        "k3.k1" = "efferthewgku hweqwrq  w"
         "k3.k2" = @{
             "ewkfh" = "srkguhdg"
         }
     }
 }
 
-$MyLogger.Log($objectLog)
+$errMsg = "Something went srong while executing action"
 
-$MyLogger.Dispose()
+$MyLogger = New-SimplePSLogger -Configuration $SimplePSLoggerConfig
+
+
+1..5 | ForEach-Object {
+    $MyLogger = New-SimplePSLogger -Name "action-$_" -Configuration $SimplePSLoggerConfig
+    1..57 | ForEach-Object {
+        if ($_ % 2 -eq 0) {
+            $MyLogger.Log($objectLog)
+        }
+        else {
+            $MyLogger.log("error", $errMsg)
+        }    
+    }
+    $MyLogger.Flush() 
+}
+
 
 
